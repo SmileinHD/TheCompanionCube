81f90069b072ac768e70e8cb5038431e12db17d6
Ethan Ary <ethanary@gmail.com>
2015-04-03 21:14:17 -0500

    Updated

--- Taxcalculator/calculatortwo.py
+++ Taxcalculator/calculatortwo.py
@@ -9,16 +9,13 @@ with open('taxes.json') as data_file:
 
 #Future feature, asks the user how many items there was for finding the
 #total of multiple items
-numberItem = float(input("How many items are there?"))
+numberItem = float(input("\nHow many items are there?: "))
 #Asks the user how much the item was
 if numberItem == 1: 
-	item = float(input("How much was the item?"))
-#Declares the tax variable
-tax = 0.00
-
+	item = float(input("\nHow much was the item?: "))
 
 #Asks the user what state they live in to determine the sales tax
-state = input("What is the abbreviation or name of your state?")
+state = input("\nWhat is the abbreviation or name of your state?: ")
 
 def jsoncheck(stateName):
 	tax = data["states"][stateName]["tax"]
@@ -28,7 +25,7 @@ jsoncheck(state)
 print(tax)
 
 #Asks the user if they want the total or the tax.
-totalOrTax = input("Do you want to see the tax or the total?: ")
+totalOrTax = input("\nDo you want to see the tax or the total?: ")
 
 realTax = tax / 100
 totalTax = realTax * item
@@ -39,10 +36,10 @@ tax = str(tax)
 
 #Prints either the total or the tax of the item which the user asked earlier.
 if totalOrTax == "total":
-	print("%.2f" % total)
+	print("\n %.2f" % total)
 
 if totalOrTax == "tax":
-	print("%.2f" % totalTax)
+	print("\n %.2f" % totalTax)
 
 input("Press any button to close.")
 
